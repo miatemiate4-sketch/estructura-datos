c660bc4f77da58f44f47af8f358d674029044202
miatemiate4-sketch <miatemiate4@gmail.com>
2026-08-22 01:56:19 +0000

    correccion

--- semana2/bolsa_lista.py
+++ semana2/bolsa_lista.py
@@ -7,17 +7,18 @@ class ElementoNoEncontradoError(Exception):
     pass
 
 class BolsaLista:
+    #Las búsquedas son O(n) porque recorre línea por línea
     def __init__(self):
         self._bolsa = []
     
     def _buscar_indice(self, elemento):
         for i, (elem, _) in enumerate(self._bolsa):
             if elem == elemento:
-                return i
-        return -1
+                return i # Encontrado → devuelve posición
+        return -1 #no encontro
     
-    def agregar(self, elemento):
-        idx = self._buscar_indice(elemento)
+    def agregar(self, elemento): # Busca si ya existe (O(n))
+        idx = self._buscar_indice(elemento)#idx=Índice (posición numérica en la lista
         if idx != -1:
             self._bolsa[idx][1] += 1
         else:
@@ -31,13 +32,14 @@ class BolsaLista:
         return self._buscar_indice(elemento) != -1  # ← CON RETURN
     
     def sacar(self, elemento):
+        #Complejidad: O(n) por búsqueda + O(n) por pop en medio de lista
         idx = self._buscar_indice(elemento)
         if idx == -1 or self._bolsa[idx][1] == 0:
             raise ElementoNoEncontradoError(f"El elemento '{elemento}' no está en la bolsa")
         
         self._bolsa[idx][1] -= 1
         if self._bolsa[idx][1] == 0:
-            self._bolsa.pop(idx)
+            self._bolsa.pop(idx)#  Elimina el par completo
     
-    def tamaño(self):
+    def tamaño(self):#Devuelve el total de elementos 
         return sum(cant for _, cant in self._bolsa)  # ← CON RETURN
